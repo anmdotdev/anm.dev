3af1917c293b3d68bc90e96f39eae9fad441645f
anmolmahatpurkar <anmolmahatpurkar@gmail.com>
2021-01-04 00:01:03 +0530

    Change chip colors

--- common/Chips.tsx
+++ common/Chips.tsx
@@ -1,37 +1,30 @@
 import { ReactNode, FC } from 'react'
 import { classnames } from 'utils/helpers'
 
-const colors = ['success', 'warn', 'error', 'purple']
-
 interface ChipProps {
   color?: string
   children: ReactNode
 }
 
-export const Chip: FC<ChipProps> = ({ color: propColor, children }) => {
-  const color = propColor || colors[Math.floor(Math.random() * colors.length)]
-
-  const classNames = {
-    success: 'border-success bg-success-lightest',
-    warn: 'border-warn bg-warn-lightest',
-    error: 'border-error bg-error-lightest',
-    purple: 'border-purple bg-purple-lightest',
-  }
-
-  return <span className={classnames('border p-1 rounded', classNames[color])}>{children}</span>
+export const Chip: FC<ChipProps> = ({ children }) => {
+  return (
+    <span className="border p-1 rounded border-gray-lighter bg-gray-lightest text-gray-darker text-xs">
+      {children}
+    </span>
+  )
 }
 
 interface ChipsProps {
   chips: {
     label: ReactNode
     color?: string
-  }[],
+  }[]
   className?: string
 }
 
 const Chips: FC<ChipsProps> = ({ chips, className }) => {
   return (
-    <div className={classnames("space-x-3", className)}>
+    <div className={classnames('space-x-3', className)}>
       {chips.map((chip, id: number) => (
         <Chip key={id} color={chip.color}>
           {chip.label}
