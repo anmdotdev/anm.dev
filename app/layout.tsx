5b07b74a4213a33f6d8d9bb93a7a5f309114f647
anmdotdev <hey@anm.dev>
2025-05-23 00:37:41 +0530

    Add stagewise toolbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import { Raleway } from 'next/font/google'
+import { StagewiseToolbar } from '@stagewise/toolbar-next'
 
 import 'tailwindcss/tailwind.css'
 
@@ -19,10 +20,17 @@ interface IRootLayoutProps {
   children: React.ReactNode
 }
 
+const stagewiseConfig = {
+  plugins: []
+}
+
 const RootLayout = ({ children }: IRootLayoutProps) => (
   <html lang="en" className={`${raleway.variable}`}>
     <body>
       <Layout>{children}</Layout>
+      {process.env.NODE_ENV === 'development' && (
+        <StagewiseToolbar config={stagewiseConfig} />
+      )}
     </body>
   </html>
 )
