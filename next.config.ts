c4b01903908564cf1ce5f1ffeaacfafc05cbd5eb
anmdotdev <hey@anm.dev>
2026-03-15 20:47:48 +0530

    Remove experimental

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  experimental: {
-    staleTimes: {
-      dynamic: 30,
-    },
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
